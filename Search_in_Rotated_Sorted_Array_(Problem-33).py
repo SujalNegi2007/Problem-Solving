32d712cefb5ecdc4ab29a602b3a28554cbe3d316
SujalNegi2007 <sujalnegi0411@gmail.com>
2026-09-18 19:53:30 +0530

    Optimize search method for rotated sorted array
    Used O(logn) to solve it. Solution Passed 31 out of 197
    Problems I faced: 
        1. Overcomplicated Things.
        2. Got Confused Over Logic

--- Search_in_Rotated_Sorted_Array_(Problem-33).py
+++ Search_in_Rotated_Sorted_Array_(Problem-33).py
@@ -7,9 +7,25 @@
 #You must write an algorithm with O(log n) runtime complexity.
 
 class Solution:
-    def search(self, nums: list[int], target: int) -> int:
-        if target not in nums:
-            return -1
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        
+        while start <= end:
+            mid = start + (end-start)//2
+
+            if nums[mid] == target:
+                return mid
+            #Checking if left half is sorted
+            if nums[start] <= nums[mid]:
+                if nums[start] <= target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            #otherwise the right half is sorted
+            else:
+                if nums[mid] < target <= nums[end]:
+                    start = mid - 1
+                else:
+                    end = mid - 1
+        return -1
