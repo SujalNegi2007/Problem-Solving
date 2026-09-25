f35301ba27d4fb489ba81956ed46ba515fd1aeca
SujalNegi2007 <sujalnegi0411@gmail.com>
2026-09-25 22:33:20 +0530

    Add Toeplitz matrix validation function
    Used O(n^2) time complexity.
    Took me 3 times to get to answer.
    It beat 100% in 0 ms in leetcode.

---
+++ Toeplitz_Matrix_(Problem-766).py
@@ -0,0 +1,12 @@
+#Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+#A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: list[list[int]]) -> bool:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i + 1 < len(matrix) and j + 1 < len(matrix[0]):
+                    if matrix[i][j] != matrix[i+1][j+1]:
+                        return False
+        return True
